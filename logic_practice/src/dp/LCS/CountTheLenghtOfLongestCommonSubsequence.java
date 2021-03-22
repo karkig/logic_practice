8f8fce05e9943de9505f2a82a36960e566124b95
karkig <kailash.karki4@gmail.com>
2021-03-22 15:30:48 +0530

    Update CountTheLenghtOfLongestCommonSubsequence.java

--- logic_practice/src/dp/LCS/CountTheLenghtOfLongestCommonSubsequence.java
+++ logic_practice/src/dp/LCS/CountTheLenghtOfLongestCommonSubsequence.java
@@ -12,19 +12,31 @@ https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
 class CountTheLenghtOfLongestCommonSubsequence
 {
   
-  public int lcs(String a[],String b[],int n,int m)
+  public int lcs()
   {
-    if(n==0||m==0)
+    String a[]="abcdd",String b[]="abdcd",int n,int m;
+    int t[n+1][m+1];
+    
+    for(int i=0;i<m+1;i++)
     {
-      return 0;
+       t[0][i] = 0;
+    }
+    for(int i=0;i<n+1;i++)
+    {
+       t[i][0] = 0;
     }
     
-    if(a[n] == b[m])
+    for(int i=1;i<n+1;i++)
     {
-       retrun 1+ lcs(a,b,n-1,m-1); 
+      for(int j=1;j<m+1;j++) 
+      {
+          if(a[i-1]==b[j-1])
+          {
+            t[i][j] = 1 + t[i-1][j-1];
+          }else
+          {
+            t[i][j] = max(t[i-1][j],t[i][j-1]);
+          }
+      }
     }
-    else
-    {
-       retrun max(lcs(a,b,n-1,m) , lcs(a,b,n,m-1));
-    } 
 }
