e0c67e02c461634444b0a8efa393474387e8b577
karkig <kailash.karki4@gmail.com>
2021-06-25 19:40:23 +0530

    Update CountTheLenghtOfLongestCommonSubString.java

--- logic_practice/src/dp/LCS/CountTheLenghtOfLongestCommonSubString.java
+++ logic_practice/src/dp/LCS/CountTheLenghtOfLongestCommonSubString.java
@@ -24,7 +24,7 @@ class CountTheLenghtOfLongestCommonSubString
             t[i][j] = 1 + t[i-1][j-1];
           }else
           {
-            t[i][j] = max(t[i-1][j],t[i][j-1]);
+            t[i][j] = 0;
           }
       }
     }
